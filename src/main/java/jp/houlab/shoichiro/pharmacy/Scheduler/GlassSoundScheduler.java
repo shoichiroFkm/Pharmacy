253a3cc7f5c81e0fdc8ed2ce4bd8d1d02a00ec14
BuildTools <unconfigured@null.spigotmc.org>
2024-06-27 00:19:07 +0900

    デバッグ完了

--- src/main/java/jp/houlab/shoichiro/pharmacy/Scheduler/GlassSoundScheduler.java
+++ src/main/java/jp/houlab/shoichiro/pharmacy/Scheduler/GlassSoundScheduler.java
@@ -1,6 +1,8 @@
 package jp.houlab.shoichiro.pharmacy.Scheduler;
 
+import jp.houlab.shoichiro.pharmacy.Pharmacy;
 import org.bukkit.Location;
+import org.bukkit.Particle;
 import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.bukkit.potion.PotionEffect;
@@ -17,11 +19,24 @@ public class GlassSoundScheduler extends BukkitRunnable {
 
     @Override
     public void run() {
+        if (player == null) {
+            return;
+        }
+        if (location == null) {
+            return;
+        }
         location.getWorld().playSound(location, Sound.BLOCK_GLASS_BREAK, 1, 1);
         location.getWorld().playSound(location, Sound.BLOCK_END_PORTAL_SPAWN, 0.3f, 1);
 
         player.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 600, 1));
         player.addPotionEffect(new PotionEffect(PotionEffectType.JUMP, 600, 1));
         player.addPotionEffect(new PotionEffect(PotionEffectType.INCREASE_DAMAGE, 600, 0));
+
+        player.getWorld().spawnParticle(Particle.ENCHANTMENT_TABLE, player.getX(), player.getY() + 2, player.getZ(), 200, 0.5, 1, 0.5, 0.5);
+        player.getWorld().spawnParticle(Particle.ASH, player.getX(), player.getY() + 2, player.getZ(), 200, 0.5, 1, 0.5, 0);
+        player.getWorld().spawnParticle(Particle.WHITE_ASH, player.getX(), player.getY() + 2, player.getZ(), 200, 0.5, 1, 0.5, 0);
+
+        new RemainScheduler(player).runTaskTimer(Pharmacy.getPlugin(), 0, 20);
+
     }
 }
